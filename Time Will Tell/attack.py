034d35dc4af3fde45950136c3cf1c96b2124e51a
Anon1984 <geoteq@gmail.com>
2024-10-27 23:58:06 +0100

    adde time will tell challenge

---
+++ Time Will Tell/attack.py
@@ -0,0 +1,146 @@
+┌──(mick㉿kali)-[~/Desktop]
+└─$ python decode.py                   
+Connection established. Starting password guess...
+Figure out the password to get the flag.
+The password is dynamic and changes every connection session.
+The connection will terminate in 90 seconds.
+: 
+Trying '00000000' - Elapsed time: 0.2031s
+Trying '10000000' - Elapsed time: 0.2030s
+Trying '20000000' - Elapsed time: 0.2031s
+Trying '30000000' - Elapsed time: 0.2029s
+Trying '40000000' - Elapsed time: 0.2030s
+Trying '50000000' - Elapsed time: 0.2029s
+Trying '60000000' - Elapsed time: 0.2029s
+Trying '70000000' - Elapsed time: 0.2029s
+Trying '80000000' - Elapsed time: 0.2030s
+Trying '90000000' - Elapsed time: 0.2029s
+Trying 'a0000000' - Elapsed time: 0.2028s
+Trying 'b0000000' - Elapsed time: 0.2029s
+Trying 'c0000000' - Elapsed time: 0.2027s
+Trying 'd0000000' - Elapsed time: 0.3028s
+Trying 'e0000000' - Elapsed time: 0.2028s
+Trying 'f0000000' - Elapsed time: 0.2028s
+Character 1 guessed: d (Partial password: d)
+
+Trying 'd0000000' - Elapsed time: 0.3033s
+Trying 'd1000000' - Elapsed time: 0.3029s
+Trying 'd2000000' - Elapsed time: 0.3032s
+Trying 'd3000000' - Elapsed time: 0.3029s
+Trying 'd4000000' - Elapsed time: 0.3031s
+Trying 'd5000000' - Elapsed time: 0.3032s
+Trying 'd6000000' - Elapsed time: 0.3031s
+Trying 'd7000000' - Elapsed time: 0.3030s
+Trying 'd8000000' - Elapsed time: 0.3029s
+Trying 'd9000000' - Elapsed time: 0.3030s
+Trying 'da000000' - Elapsed time: 0.3032s
+Trying 'db000000' - Elapsed time: 0.5037s
+Trying 'dc000000' - Elapsed time: 0.3036s
+Trying 'dd000000' - Elapsed time: 0.3030s
+Trying 'de000000' - Elapsed time: 0.3031s
+Trying 'df000000' - Elapsed time: 0.3033s
+Character 2 guessed: b (Partial password: db)
+
+Trying 'db000000' - Elapsed time: 0.5036s
+Trying 'db100000' - Elapsed time: 0.4034s
+Trying 'db200000' - Elapsed time: 0.4033s
+Trying 'db300000' - Elapsed time: 0.4032s
+Trying 'db400000' - Elapsed time: 0.4032s
+Trying 'db500000' - Elapsed time: 0.4032s
+Trying 'db600000' - Elapsed time: 0.4031s
+Trying 'db700000' - Elapsed time: 0.4032s
+Trying 'db800000' - Elapsed time: 0.4072s
+Trying 'db900000' - Elapsed time: 0.4035s
+Trying 'dba00000' - Elapsed time: 0.4033s
+Trying 'dbb00000' - Elapsed time: 0.4032s
+Trying 'dbc00000' - Elapsed time: 0.4033s
+Trying 'dbd00000' - Elapsed time: 0.4035s
+Trying 'dbe00000' - Elapsed time: 0.4034s
+Trying 'dbf00000' - Elapsed time: 0.4032s
+Character 3 guessed: 0 (Partial password: db0)
+
+Trying 'db000000' - Elapsed time: 0.5038s
+Trying 'db010000' - Elapsed time: 0.5032s
+Trying 'db020000' - Elapsed time: 0.5034s
+Trying 'db030000' - Elapsed time: 0.6036s
+Trying 'db040000' - Elapsed time: 0.5036s
+Trying 'db050000' - Elapsed time: 0.5037s
+Trying 'db060000' - Elapsed time: 0.5036s
+Trying 'db070000' - Elapsed time: 0.5037s
+Trying 'db080000' - Elapsed time: 0.5037s
+Trying 'db090000' - Elapsed time: 0.5035s
+Trying 'db0a0000' - Elapsed time: 0.5034s
+Trying 'db0b0000' - Elapsed time: 0.5036s
+Trying 'db0c0000' - Elapsed time: 0.5036s
+Trying 'db0d0000' - Elapsed time: 0.5035s
+Trying 'db0e0000' - Elapsed time: 0.5037s
+Trying 'db0f0000' - Elapsed time: 0.5036s
+Character 4 guessed: 3 (Partial password: db03)
+
+Trying 'db030000' - Elapsed time: 0.6039s
+Trying 'db031000' - Elapsed time: 0.7041s
+Trying 'db032000' - Elapsed time: 0.6039s
+Trying 'db033000' - Elapsed time: 0.6037s
+Trying 'db034000' - Elapsed time: 0.6040s
+Trying 'db035000' - Elapsed time: 0.6038s
+Trying 'db036000' - Elapsed time: 0.6040s
+Trying 'db037000' - Elapsed time: 0.6035s
+Trying 'db038000' - Elapsed time: 0.6037s
+Trying 'db039000' - Elapsed time: 0.6036s
+Trying 'db03a000' - Elapsed time: 0.6035s
+Trying 'db03b000' - Elapsed time: 0.6034s
+Trying 'db03c000' - Elapsed time: 0.6035s
+Trying 'db03d000' - Elapsed time: 0.6036s
+Trying 'db03e000' - Elapsed time: 0.6039s
+Trying 'db03f000' - Elapsed time: 0.6036s
+Character 5 guessed: 1 (Partial password: db031)
+
+Trying 'db031000' - Elapsed time: 0.7039s
+Trying 'db031100' - Elapsed time: 0.8040s
+Trying 'db031200' - Elapsed time: 0.7037s
+Trying 'db031300' - Elapsed time: 0.7038s
+Trying 'db031400' - Elapsed time: 0.7038s
+Trying 'db031500' - Elapsed time: 0.7074s
+Trying 'db031600' - Elapsed time: 0.7037s
+Trying 'db031700' - Elapsed time: 0.7037s
+Trying 'db031800' - Elapsed time: 0.7041s
+Trying 'db031900' - Elapsed time: 0.7039s
+Trying 'db031a00' - Elapsed time: 0.7038s
+Trying 'db031b00' - Elapsed time: 0.7038s
+Trying 'db031c00' - Elapsed time: 0.7038s
+Trying 'db031d00' - Elapsed time: 0.7048s
+Trying 'db031e00' - Elapsed time: 0.7038s
+Trying 'db031f00' - Elapsed time: 0.7040s
+Character 6 guessed: 1 (Partial password: db0311)
+
+Trying 'db031100' - Elapsed time: 0.8039s
+Trying 'db031110' - Elapsed time: 0.8039s
+Trying 'db031120' - Elapsed time: 0.8040s
+Trying 'db031130' - Elapsed time: 0.8039s
+Trying 'db031140' - Elapsed time: 0.8038s
+Trying 'db031150' - Elapsed time: 0.8041s
+Trying 'db031160' - Elapsed time: 0.8041s
+Trying 'db031170' - Elapsed time: 0.8040s
+Trying 'db031180' - Elapsed time: 0.8040s
+Trying 'db031190' - Elapsed time: 0.8039s
+Trying 'db0311a0' - Elapsed time: 0.8037s
+Trying 'db0311b0' - Elapsed time: 0.8041s
+Trying 'db0311c0' - Elapsed time: 0.8040s
+Trying 'db0311d0' - Elapsed time: 0.8042s
+Trying 'db0311e0' - Elapsed time: 0.9042s
+Trying 'db0311f0' - Elapsed time: 0.8041s
+Character 7 guessed: e (Partial password: db0311e)
+
+Trying 'db0311e0' - Elapsed time: 0.9135s
+Trying 'db0311e1' - Elapsed time: 0.9041s
+Trying 'db0311e2' - Elapsed time: 0.9041s
+Trying 'db0311e3' - Elapsed time: 0.9043s
+Trying 'db0311e4' - Elapsed time: 0.9042s
+Trying 'db0311e5' - Elapsed time: 0.9040s
+Trying 'db0311e6' - Elapsed time: 0.9042s
+Trying 'db0311e7' - Elapsed time: 0.9042s
+Trying 'db0311e8' - Elapsed time: 0.9040s
+Trying 'db0311e9' - Elapsed time: 0.9040s
+Trying 'db0311ea' - Elapsed time: 1.0069s
+Password found: db0311ea
+Well done! Here's your flag: flag{ab6962e29ed608c0710dbf2910f358d5}
